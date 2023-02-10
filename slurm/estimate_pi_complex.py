7213a329ab6f7a790ea2fdca7a61503e448934a7
Joe Stitt <jstitty@gmail.com>
2023-02-10 17:47:08 +0000

    updated slurm lab

--- slurm/estimate_pi_complex.py
+++ slurm/estimate_pi_complex.py
@@ -5,18 +5,17 @@ import sys
 
 task_id = int(os.environ['SLURM_ARRAY_TASK_ID']) # grab the slurm_id
 random.seed(task_id) # use task id to get a random number
-trial_num = 1000 # number of trials to estimate pi
+trial_num = 10000 # number of trials to estimate pi
 
 num_of_successes = 0 # start the count for number of trials that meet the criteria
 
 for i in range(trial_num):
-    x = np.random.normal(0,1)
-    y = np.random.normal(0,1)
+    x = np.random.random()
+    y = np.random.random()
     if (x**2) + (y**2) <=1: # sum of squares
         num_of_successes += 1
         
-pi_estimate = 4 * (num_of_successes / trial_num) # calculate the estimate
-
+pi_estimate = 4 * float(num_of_successes/trial_num) # calculate the estimate
 filename = f"result_{task_id}.txt" # file name with task id
 with open(filename, 'w') as f:
     f.write(str(pi_estimate)) # write the estimate
